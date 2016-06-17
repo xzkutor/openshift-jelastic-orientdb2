be39e3ea0d35bae37f1435022e81236f9c7594e3
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2016-06-17 18:25:30 +0300

    Update generate_optimal_configs.sh

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -10,11 +10,8 @@ if [ -e ${OPENSHIFT_ORIENTDB_DIR}/versions/${Version}/bin/variablesparser.sh ]; 
       . ${OPENSHIFT_ORIENTDB_DIR}/versions/${Version}/bin/variablesparser.sh
 fi
 
-[ -z "$XMS" ] && { XMS=32m; }
-memory_total=`free -m | grep Mem | awk '{print $2}'`;
-[ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}m";  }
 let MAXDISKCACHE=memory_total-67;
 
-$SED -i "s/-Xmx\([0-9]*[mM]\)*/-Xmx${XMX}/" $ORIENTDB_RUN_SCRIPT;
-$SED -i "s/-Xms\([0-9]*[mM]\)*/-Xms${XMS}/" $ORIENTDB_RUN_SCRIPT;
+$SED -i "s/-Xmx\([0-9]*[mM]\)*/${XMX}/" $ORIENTDB_RUN_SCRIPT;
+$SED -i "s/-Xms\([0-9]*[mM]\)*/${XMS}/" $ORIENTDB_RUN_SCRIPT;
 $SED -i "s/\"-Dstorage.diskCache.bufferSize=[0-9]*\"/\"-Dstorage.diskCache.bufferSize=$MAXDISKCACHE\"/" $ORIENTDB_RUN_SCRIPT;
